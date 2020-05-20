d2fbecb0d0d9928c411699dd442809b737644ae4
guplem <11029629+guplem@users.noreply.github.com>
2020-05-20 13:33:32 +0200

    Displaying image

--- GlideProject/app/src/main/java/com/tecnocampus/glideproject/MainActivity.java
+++ GlideProject/app/src/main/java/com/tecnocampus/glideproject/MainActivity.java
@@ -3,12 +3,36 @@ package com.tecnocampus.glideproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+
+        // DIFFERENT OPTIONS to show the image
+        //loadResourceWithDefaultLibrary();
+        loadResourceWithGlideLocal();
+        //loadResourceWithGlideURL();
+    }
+
+    private void loadResourceWithDefaultLibrary() {
+        imageView.setImageResource(R.drawable.lion);
+    }
+
+    private void loadResourceWithGlideLocal() {
+        Glide.with(this).load(R.drawable.lion).into((imageView));
+    }
+
+    private void loadResourceWithGlideURL() { // Needs Internet Permission
+        Glide.with(this).load("https://lionsclubs.co/Public/wp-content/uploads/2018/06/xcropped-lions-web.png.pagespeed.ic.pgwrox6ThR.jpg").into(imageView);
     }
 }
