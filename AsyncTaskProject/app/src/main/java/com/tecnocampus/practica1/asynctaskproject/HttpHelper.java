cc626204e35c3975e5f036b4952224046efafe26
guplem <11029629+guplem@users.noreply.github.com>
2020-05-20 10:38:37 +0200

    Created Project

---
+++ AsyncTaskProject/app/src/main/java/com/tecnocampus/practica1/asynctaskproject/HttpHelper.java
@@ -0,0 +1,10 @@
+package com.tecnocampus.practica1.asynctaskproject;
+
+import android.os.AsyncTask;
+
+public class HttpHelper extends AsyncTask {
+    @Override
+    protected Object doInBackground(Object[] objects) {
+        return null;
+    }
+}
