d27fd39437bef9e5df29e4bf08e6bfb66ae01687
Guillem Poy <guplem@gmail.com>
2020-06-03 09:45:42 +0200

    simple light sensor report

--- SensorProject/app/src/main/java/com/tecnocampus/sensorproject/MainActivity.java
+++ SensorProject/app/src/main/java/com/tecnocampus/sensorproject/MainActivity.java
@@ -2,13 +2,63 @@ package com.tecnocampus.sensorproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    SensorManager sensorManager;
+    private Sensor sensor;
+    private TextView resultText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        resultText = findViewById(R.id.results_view);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        if (sensorManager != null && sensorManager.getDefaultSensor((Sensor.TYPE_LIGHT)) != null){
+            sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        } else {
+            Log.d("CustomTag", "Light sensor not available");
+        }
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (sensor != null) {
+            sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float lux = event.values[0];
+        String oldResult = resultText.getText().toString();
+        resultText.setText(lux + "\n" + oldResult);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
