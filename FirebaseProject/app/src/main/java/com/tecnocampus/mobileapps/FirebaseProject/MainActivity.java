faf0d30e0927e14e297bbd4e14873a342be7c965
Guillem Poy <guplem@gmail.com>
2020-06-10 11:32:53 +0200

    Created notification channel

--- FirebaseProject/app/src/main/java/com/tecnocampus/mobileapps/FirebaseProject/MainActivity.java
+++ FirebaseProject/app/src/main/java/com/tecnocampus/mobileapps/FirebaseProject/MainActivity.java
@@ -2,13 +2,30 @@ package com.tecnocampus.mobileapps.FirebaseProject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static String CHANNEL_ID;
+    private static int NOTIFICATION_ID = 42;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        createNotificationsChannel();
+    }
+
+    private void createNotificationsChannel() {
+        if (Build.VERSION.SDK_INT>= Build.VERSION_CODES.O){
+            String tag = "Notif. Test";
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, tag, NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
     }
 }
